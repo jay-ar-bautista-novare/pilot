c958cc9f2855760b4ca436c64b552c3474c3e394
jay-ar-bautista-novare <109153868+jay-ar-bautista-novare@users.noreply.github.com>
2022-08-01 23:58:08 +0800

    Add Test Coverage (#2)
    * Add Test Coverage
    
    * Increase Minimum coverage to 95%
    
    * Set minimum coverage to 90%

---
+++ src/test/java/com/sofa/pilot/config/PropertyLoaderTest.java
@@ -0,0 +1,20 @@
+package com.sofa.pilot.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class PropertyLoaderTest {
+
+	private static final String TEST_KEY = "MONGO_HOST";
+	private static final String TEST_VALUE = "localhost";
+
+	@Test
+	void givenPropertyLoaderWhenGetValueOfIsInvokedThenExpectedResultIsReturned() {
+		PropertyLoader propertyLoader = new PropertyLoader();
+
+		String result = propertyLoader.getValueOf(TEST_KEY);
+
+		assertEquals(TEST_VALUE, result);
+	}
+}
